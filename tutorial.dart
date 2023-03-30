3f5cdc90ee9a94bf1baba196f3f2c0e761b5ab9e
Samuel Odukoya <samuelodukoya14@gmail.com>
2023-03-30 01:32:15 +0100

    understanding dart more

---
+++ tutorial.dart
@@ -0,0 +1,83 @@
+// Creating and Calling the Function
+// void Samuel(int anumber) {
+//   print('The Number is $anumber');
+// }
+
+// Assigning the function to Number
+// void main() {
+//   dynamic number = 45;
+//   Samuel(number);
+// }
+
+// void main() {
+// Using camelcase for the firstName, CamelCase is where we have the rest of every first word Capitalized
+// Forgot to add the '' quotation since it was a string
+//   var firstName = 'samuel';
+//   Object lastName = 'odukoya';
+//   dynamic userName = 'TechaDot';
+//   print(firstName);
+//   print(lastName);
+//   print(userName);
+// }
+
+// void main() {
+// int lineCount = 0;
+// assert(lineCount == null);
+// int lineCount;
+
+// if (weLikeToCount) {
+//   lineCount = countLines();
+// } else {
+//   lineCount = 0;
+// }
+
+// print(lineCount);
+
+// late String description;
+
+// void main() {
+//   description = 'Feijoada!';
+//   print(description);
+// }
+// }
+
+// void main() {
+//   final name = 'bob';
+//   final String nickname = 'bobby';
+//   print(name);
+//   print(nickname);
+// }
+
+// void main() {
+//   for (int x = 0; x < 5; x++) {
+//     print('hello ${x + 1}');
+//   }
+// }
+
+// void printInteger(int aNumbers) {
+//   print('The number is $aNumbers.');
+// }
+
+// void main() {
+//   var aNumbers = 20;
+//   printInteger(aNumbers);
+// }
+
+// void main() {
+//   int lineCounts;
+//   if (weLikeToCounts) {
+//     lineCounts = countLines();
+//   } else {
+//     lineCounts = 0;
+//   }
+//   print(lineCounts)
+// }
+void main() {
+  // print('hello_World');
+  // int ifn = 10;
+  // print(ifn);
+
+  var firstName = 'samuel';
+  var secondName = 'odukoya';
+  print('My FirstName is ${firstName}, and my Surname is $secondName');
+}
