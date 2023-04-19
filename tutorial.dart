16b1345448c469ba0008936ac0f6c3603a2c2cef
Samuel Odukoya <samuelodukoya14@gmail.com>
2023-04-19 22:16:44 +0100

    did some tutorial

--- tutorial.dart
+++ tutorial.dart
@@ -72,6 +72,7 @@
 //   }
 //   print(lineCounts)
 // }
+
 void main() {
   // print('hello_World');
   // int ifn = 10;
@@ -89,4 +90,61 @@ void main() {
 
   // double pi = 3.14;
   // print(pi);
+
+  // var name = 'samuel';
+  // print(name);
+
+  // bool asleep = true;
+  // print(asleep);
+
+  // var groceries = [1, 2, 4];
+  // print(groceries);
+
+  // var students = {'names': 'samuel', 'age': 22, 'Branch': 'Engineering'};
+  // print(students['names']);
+
+  // var hearts_symbol = '\u2665';
+  // print(hearts_symbol);
+
+  // int counts;
+  // assert(counts == null);
+
+  // var a = 'samuel';
+  // var b = 'odukoya';
+  // print(""""My name is $a, My sunrname is $b,
+  // My full name is $a  $b, My name is also Otherwise known as ${a + b}""");
+
+  // var d = num.parse('4');
+  // double pi = 3.147;
+  // double rest = 4 * pi * d;
+  // print('it is = ${(rest)}');
+
+  // var list1 = [13, 14, 2, 3];
+  // print(list1);
+
+  // var list1 = new List.filled(5, null, growable: false);
+  // list1[0] = 14;
+  // list1[1] = 16;
+  // list1[2] = 11;
+  // list1[3] = 19;
+  // list1[4] = 12;
+  // print(list1);
+
+//   String fname = 'samuel';
+//   String lname = 'odukoya';
+//   print('my First Name is $fname, While my Last Name is $lname');
+//   print('My mama First Name na ' + fname + 'While her last name na ' + lname);
+//   print(fname.toUpperCase());
+//   String sentence =
+//       '''I am samuel odukoya, \n i am destined to be great in life in all ramfications, i might experience some difficulties or delays but its going to be fine ''';
+//   print(sentence);
+
+  // double a = 4;
+  // double b = 10;
+  // double c = a + b;
+  // print(c.toString());
+
+  String? value = 'text';
+  value = null;
+  print(value);
 }
